a3328c5e13b1d93c88e00273e9186303be633cbe
alessandrordgs <ale.silva1120@gmail.com>
2021-12-03 20:45:29 -0300

    teste digital

--- src/config/database.js
+++ src/config/database.js
@@ -1,25 +1,24 @@
 module.exports = {
-    development: {
-      "username": "alessandrordgs",
-      "password": "root0308",
-      "database": "proffy",
-      "host": "mysql796.umbler.com",
-      "port":"41890",
-      "dialect": "mysql"
-    },
-    test: {
-      "username": "root",
-      "password":"",
-      "database": "database_test",
-      "host": "127.0.0.1",
-      "dialect": "mysql"
-    },
-    production: {
-      "username": "root",
-      "password":"",
-      "database": "database_production",
-      "host": "127.0.0.1",
-      "dialect": "mysql"
-    }
-  
-}
+  development: {
+    username: "alessandroprojet",
+    password: "root0308",
+    database: "home_projeti",
+    host: "mysql524.umbler.com",
+    port: 41890,
+    dialect: "mysql",
+  },
+  test: {
+    username: "root",
+    password: "",
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    username: "root",
+    password: "",
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+};
